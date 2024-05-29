53576ffd8f39f9579e2dd3f090c27d717c23e221
Marvin Ola <olamarvin40@gmail.com>
2024-05-28 22:09:24 -0600

    aprendizaje de command in line arguments

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ class Program
     static int Main(string[] args)
     {
         // Display a simple message to the user.
-        Console.WriteLine("***** My First C# App *****");
-        Console.WriteLine("Hello World!");
+        //Console.WriteLine("***** My First C# App *****");
+        //Console.WriteLine("Hello World!");
 
         //Procesing command line arguments
         for(int i = 0; i < args.Length; i++)
@@ -16,7 +16,16 @@ class Program
         Console.WriteLine("Number of processors: {0}",
         Environment.ProcessorCount);
         Console.WriteLine(".NET Version: {0}",Environment.Version);
-        return -1;
 
+        // Print out the drives on this machine,
+        // and other interesting details.
+        foreach (string drive in Environment.GetLogicalDrives())
+        Console.WriteLine("Drive: {0}", drive);
+        Console.WriteLine("OS: {0}", Environment.OSVersion);
+        Console.WriteLine("Number of processors: {0}",
+        Environment.ProcessorCount);
+        Console.WriteLine(".NET Version: {0}",
+        Environment.Version);
+        return -1;
     }
 }
